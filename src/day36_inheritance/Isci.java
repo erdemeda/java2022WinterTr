4ecc3ac6967d21b3ecb0eba5efbab5531a80693e
Eda ERDEM <cngz.eda@gmail.com>
2022-04-05 20:15:16 +0300

    5.04.2022

---
+++ src/day36_inheritance/Isci.java
@@ -0,0 +1,13 @@
+package day36_inheritance;
+
+public class Isci extends Muhasebe{
+
+    protected String isciStatu="Surekli Isci";
+    protected void mesai(){
+        System.out.println("ısciler gunluk 8 saat ve haftada 5 gun calisir");
+    }
+
+
+
+
+}

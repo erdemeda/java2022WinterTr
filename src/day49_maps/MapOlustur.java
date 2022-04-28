b579de7158729d0e05767fbf1ebc7f17e544c22c
Eda ERDEM <cngz.eda@gmail.com>
2022-04-28 23:46:04 +0300

    28.04.2022

--- src/day49_maps/MapOlustur.java
+++ src/day49_maps/MapOlustur.java
@@ -8,9 +8,11 @@ public class MapOlustur {
 
         Map<Integer,String> sinifList = new HashMap<>();
 
-        sinifList.put(101,"Ali, Can, Dev");
-        sinifList.put(102,"Veli, Yan, QA");
-        sinifList.put(103,"Ali, Yan, C#");
+        sinifList.put(101,"Ali, Can, Dev,1453");
+        sinifList.put(102,"Veli, Yan, QA,1990");
+        sinifList.put(103,"Ali, Yan, C#,2000");
+        sinifList.put(104,"Ayse, Kan, C#,2001");
+        sinifList.put(105,"Fatma, Han, Java,2002");
 
         return sinifList;
     }
